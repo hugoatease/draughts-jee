9ade33065c119d11c80686aa611b72f478950cae
Hugo Caille <hugo.caille@isen-lille.fr>
2017-01-09 16:31:53 +0100

    Pawn class implementation

---
+++ draughts-jee-core/src/Pawn.java
@@ -0,0 +1,39 @@
+public class Pawn {
+    public enum PawnType {
+        PAWN,
+        QUEEN
+    }
+
+    public enum PawnColor {
+        BLACK,
+        WHITE
+    }
+
+    private PawnType pawnType = PawnType.PAWN;
+    private PawnColor pawnColor;
+
+    public Pawn(PawnType pawnType, PawnColor pawnColor) {
+        this.pawnType = pawnType;
+        this.pawnColor = pawnColor;
+    }
+
+    public Pawn(PawnColor pawnColor) {
+        this.pawnColor = pawnColor;
+    }
+
+    public PawnType getPawnType() {
+        return pawnType;
+    }
+
+    public void setPawnType(PawnType pawnType) {
+        this.pawnType = pawnType;
+    }
+
+    public PawnColor getPawnColor() {
+        return pawnColor;
+    }
+
+    public void setPawnColor(PawnColor pawnColor) {
+        this.pawnColor = pawnColor;
+    }
+}
