443e57fd7691220b2f602e19634d0bb93ccb1e96
Hugo Caille <hugo.caille@isen-lille.fr>
2017-02-27 05:00:14 +0100

    Test player joining

--- draughts-jee-jpa/src/test/java/me/caille/draughts/repositories/GameRepositoryTest.java
+++ draughts-jee-jpa/src/test/java/me/caille/draughts/repositories/GameRepositoryTest.java
@@ -1,23 +1,34 @@
 package me.caille.draughts.repositories;
 
 import me.caille.draughts.entities.GameEntity;
+import me.caille.draughts.entities.PlayerEntity;
+import org.ajbrown.namemachine.NameGenerator;
 import org.junit.After;
 import org.junit.Test;
 
 import javax.ejb.embeddable.EJBContainer;
 
+import static javassist.util.proxy.ProxyFactory.nameGenerator;
 import static org.junit.Assert.*;
 
 public class GameRepositoryTest {
     private GameRepository repository;
+    private PlayerRepository playerRepository;
     private EJBContainer ejbContainer;
     private GameEntity game;
+    private NameGenerator nameGenerator;
+    private PlayerEntity whitePlayer;
+    private PlayerEntity blackPlayer;
 
     @org.junit.Before
     public void setUp() throws Exception {
         ejbContainer = EJBContainer.createEJBContainer();
         repository = (GameRepository) ejbContainer.getContext().lookup("java:global/draughts-jee-jpa/GameRepository");
+        playerRepository = (PlayerRepository) ejbContainer.getContext().lookup("java:global/draughts-jee-jpa/PlayerRepository");
         game = repository.createGame();
+        nameGenerator = new NameGenerator();
+        whitePlayer = playerRepository.create(nameGenerator.generateName().toString());
+        blackPlayer = playerRepository.create(nameGenerator.generateName().toString());
     }
 
     @After
@@ -35,5 +46,19 @@ public class GameRepositoryTest {
     public void canRetrieveGame() {
         GameEntity retrieved = repository.getById(game.getId());
         assertEquals(game, retrieved);
+    }
+
+    @Test
+    public void canJoinWhitePlayer() {
+        game.setWhitePlayer(whitePlayer);
+        GameEntity retrieved = repository.getById(game.getId());
+        assertEquals(game.getWhitePlayer(), retrieved.getWhitePlayer());
+    }
+
+    @Test
+    public void canJoinBlackPlayer() {
+        game.setBlackPlayer(blackPlayer);
+        GameEntity retrieved = repository.getById(game.getId());
+        assertEquals(game.getBlackPlayer(), retrieved.getBlackPlayer());
     }
 }
